91ab159676238aaba36f4cd6c337fc990e80c8ac
Brian Cohan <briancohan@gmail.com>
2023-06-17 21:43:14 -0400

    remove unused parts in select

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -5,24 +5,6 @@ import clsx from 'clsx'
 
 import Label from '@/app/server_components/Label'
 
-const people = [
-  { id: 1, name: 'Wade Cooper' },
-  { id: 2, name: 'Arlene Mccoy' },
-  { id: 3, name: 'Devon Webb' },
-  { id: 4, name: 'Tom Cook' },
-  { id: 5, name: 'Tanya Fox' },
-  { id: 6, name: 'Hellen Schmidt' },
-  { id: 7, name: 'Caroline Schultz' },
-  { id: 8, name: 'Mason Heaney' },
-  { id: 9, name: 'Claudie Smitham' },
-  { id: 10, name: 'Emil Schaefer' },
-]
-
-type selectOption = {
-  value: string | number
-  text: string
-}
-
 interface SelectProps {
   name: string
   options: string[]
