03cbf134fcb7f04a1cc279789ba82e23b01688b9
Brian Cohan <briancohan@gmail.com>
2023-06-17 22:13:20 -0400

    fix form element focus color

--- src/components/BaseInput.tsx
+++ src/components/BaseInput.tsx
@@ -21,7 +21,7 @@ const BaseInput: React.FC<BaseInputProps> = ({ name, type = 'text', value = '', 
       slotProps={{
         input: {
           className:
-            'block w-full rounded-md border-0 py-1.5 px-4 text-gray-100 shadow-sm ring-1 bg-zinc-700 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-orange-600 sm:text-sm sm:leading-6',
+            'block w-full rounded-md border-0 py-1.5 px-4 text-gray-100 shadow-sm ring-1 bg-zinc-700 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sole-green sm:text-sm sm:leading-6',
           onChange: (e) => onChange(e.target.value),
         },
       }}
--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -19,7 +19,7 @@ const Select: React.FC<SelectProps> = ({ name, options, value, onChange }) => {
         <div>
           {name && <Label name='name'>{name}</Label>}
           <div className='relative mt-2'>
-            <Listbox.Button className='relative w-full cursor-default rounded-md bg-zinc-700 py-1.5 pl-3 pr-10 text-left text-gray-100 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6'>
+            <Listbox.Button className='relative w-full cursor-default rounded-md bg-zinc-700 py-1.5 pl-3 pr-10 text-left text-gray-100 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-sole-green sm:text-sm sm:leading-6'>
               <span className='block truncate'>{value}</span>
               <span className='absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none'>
                 <ChevronUpDownIcon className='w-5 h-5 text-gray-400' aria-hidden='true' />
@@ -38,7 +38,7 @@ const Select: React.FC<SelectProps> = ({ name, options, value, onChange }) => {
                     key={option}
                     className={({ active }) =>
                       clsx({
-                        'bg-indigo-600 text-white': active,
+                        'bg-sole-green text-white': active,
                         'text-gray-100': !active,
                         'relative cursor-default select-none py-2 pl-3 pr-9': true,
                       })
@@ -59,7 +59,7 @@ const Select: React.FC<SelectProps> = ({ name, options, value, onChange }) => {
                           <span
                             className={clsx({
                               'text-white': active,
-                              'text-indigo-600': !active,
+                              'text-sole-green': !active,
                               'absolute inset-y-0 right-0 flex items-center pr-4': true,
                             })}>
                             <CheckIcon className='w-5 h-5' aria-hidden='true' />
