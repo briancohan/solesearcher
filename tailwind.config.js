98d8d181a5a24ba488aa4ce4c8c27bf86408700e
Brian Cohan <briancohan@gmail.com>
2023-06-18 20:47:10 -0400

    remove tw line-clamp

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,10 +17,5 @@ module.exports = {
       },
     },
   },
-  plugins: [
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/line-clamp'),
-    require('@tailwindcss/aspect-ratio'),
-  ],
+  plugins: [require('@tailwindcss/typography'), require('@tailwindcss/forms'), require('@tailwindcss/aspect-ratio')],
 }
