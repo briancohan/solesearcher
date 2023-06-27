408274b739d362695733ba11003e22728f7f55d6
Brian Cohan <briancohan@gmail.com>
2023-06-27 10:16:04 -0400

    set footer image as priority

--- src/app/server_components/Footer.tsx
+++ src/app/server_components/Footer.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 const Footer: React.FC = () => {
   return (
     <div className='flex flex-col items-center gap-4 p-8 mt-12'>
-      <Image src='/sole-searcher-logo.webp' alt='Sole Searcher Logo' width={250} height={250} />
+      <Image src='/sole-searcher-logo.webp' alt='Sole Searcher Logo' width={250} height={250} priority />
       <p className='text-zinc-300'>
         Produced for{' '}
         <a className='text-sole-tan' href='http://tracking-books.com'>
